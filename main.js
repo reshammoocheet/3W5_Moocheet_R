d53eb48212ab78ae2b3d2a0e87673a36e4dcee64
Resham Moocheet <78217504+reshammoocheet@users.noreply.github.com>
2021-12-07 14:21:17 -0500

    Baseline for website
    HTML elements configured: -selectors - dates.

---
+++ main.js
@@ -0,0 +1,17 @@
+let JSONex = [{"SegmentId":4,"SegmentName":"Deux-Montagnes-Bois-Franc","Name":"Sainte-Dorothée","StationId":18},
+{"SegmentId":4,"SegmentName":"Deux-Montagnes-Bois-Franc","Name":"Île-Bigras","StationId":17},
+{"SegmentId":4,"SegmentName":"Deux-Montagnes-Bois-Franc","Name":"Pierrefonds-Roxboro","StationId":16},
+{"SegmentId":4,"SegmentName":"Deux-Montagnes-Bois-Franc","Name":"Sunnybrooke","StationId":15},
+{"SegmentId":4,"SegmentName":"Deux-Montagnes-Bois-Franc","Name":"Bois-Franc","StationId":14},
+{"SegmentId":7,"SegmentName":"Bois-Franc-YUL-Aéroport-Montréal-Trudeau","Name":"Bois-Franc","StationId":14},
+{"SegmentId":7,"SegmentName":"Bois-Franc-YUL-Aéroport-Montréal-Trudeau","Name":"Marie-Curie","StationId":25}];
+
+
+let roleSelect = document.getElementById("role");
+
+roleSelect.addEventListener("click", setRole);
+
+function setRole() {
+
+}
+
